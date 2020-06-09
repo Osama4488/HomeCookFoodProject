8b04792b5b2f45de3cf2e8b56b9c70e84dca1f34
osamaali <syed.osama.ali.96@gmail.com>
2020-06-09 18:40:17 +0500

    Cart activity task done

--- app/src/main/java/com/example/myfirebasejavaproject/ActivitiesNew/User/CartActivity.java
+++ app/src/main/java/com/example/myfirebasejavaproject/ActivitiesNew/User/CartActivity.java
@@ -35,12 +35,16 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
 public class CartActivity extends AppCompatActivity {
 
     static int TotalQuantity=0;
+    static String previousValue ="0";
+    static int puraniValue =0;
+    static int quantityKeyInteger = 0;
     static boolean checkQantity = false;
     static int TotalBill;
     int size;
@@ -53,16 +57,19 @@ public class CartActivity extends AppCompatActivity {
     DatabaseReference refrence;
     SharedPreferences prefs;
     String _KEY;
+    HashSet<String> hashet;
     TextView showQuanity,showBill;
     HashMap<String,Cart_Model> mList;
     RelativeLayout btnPlaceOrder;
     List<Integer> keys;
     List<Integer> quantityKeys;
+    //HashMap<String,Integer> quantityKeysNew;
     static int totalBill;
     Appointment_Model apointmentModel;
     List<String> cartIdList;
     ImageView cartBackIconn;
-    HashMap<Integer,String> quantityList;
+    HashMap<String,String> quantityList;
+    HashMap<String,String> quantityKeysNew;
     HashMap<Integer,String> priceList;
 
     @Override
@@ -77,17 +84,19 @@ public class CartActivity extends AppCompatActivity {
         mList = new HashMap<>();
         keys = new ArrayList<>();
         quantityKeys = new ArrayList<>();
+        quantityKeysNew = new HashMap<>();
         cartIdList = new ArrayList<>();
         quantityList = new HashMap<>();
         priceList = new HashMap<>();
         btnPlaceOrder = findViewById(R.id.billLayout);
         cartBackIconn = findViewById(R.id.cartBackIcon);
         postion_list = new ArrayList<>();
+        hashet = new HashSet<>();
         cartBackIconn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-                //startActivity(new Intent(CartActivity.this,Menu_Profile.class));
+                //startActivi-ty(new Intent(CartActivity.this,Menu_Profile.class));
             }
         });
 
@@ -125,7 +134,8 @@ public class CartActivity extends AppCompatActivity {
                                 SharedPreferences editor = getSharedPreferences("clickedProfile",Context.MODE_PRIVATE);
                                 String cookerId = editor.getString("uid","");
                                 apointmentModel.setCookerId(cookerId);
-                                apointmentModel.setTotalBill(Integer.toString(TotalBill));
+                                apointmentModel.setTotalBill(Integer.toString(totalBill));
+                               // apointmentModel.setTotalBill(Integer.toString(TotalBill));
                                 SharedPreferences prefs = getSharedPreferences(UserHelperClass.shared, MODE_PRIVATE);
                                 String usersAddress = prefs.getString("address", null);
                                 apointmentModel.setAddress(usersAddress);
@@ -158,7 +168,7 @@ public class CartActivity extends AppCompatActivity {
                          UserHelperClass.path.child(_KEY).child("Cart").child(cartIdList.get(i)).setValue(null);
                     }
 
-
+                    totalBill = 0;
                     Toast.makeText(CartActivity.this, "Your Order has been Placed", Toast.LENGTH_LONG).show();
                     startActivity(new Intent(CartActivity.this, UserDashboard.class));
                 }
@@ -251,134 +261,253 @@ public class CartActivity extends AppCompatActivity {
             priceList.remove(Integer.valueOf(position));
         } else {
 
-
-            quantityList.put(Integer.valueOf(position), qty);
+            quantityList.put(position, qty);
             priceList.put(Integer.valueOf(position), price);
             postion_list.add(Integer.valueOf(position));
             mList.put(position, model);
-            //TotalQuantity = 0;
+            //quantityKeysNew.put(position,position);
+            String c = "";
+            quantityKeys = new ArrayList<>();
+            TotalQuantity = 0;
             TotalBill = 0;
+//
+//            for(int j =0; j < mList.size();j++){
+//
+//                    if(keys.equals(keys.get(j))){
+//                        continue;
+//                    }
+//                    else {
+//                        quantityKeysNew.put(position,mList.keySet().toString());
+//                    }
+//            }
 
+            for ( String key : mList.keySet() ) {
 
+                boolean k = keys.contains(key);
+                boolean bb = hashet.contains(key);
 
-            for ( int key : quantityList.keySet() ) {
-                quantityKeys.add(Integer.valueOf(key));
-                break;
+                if(hashet.contains(key)){
+                    continue;
+                }
+                else {
+                    hashet.add(key);
+                    keys.add(Integer.valueOf(key));
+                }
+
+               // quantityKeysNew.put(position,key);
 
             }
 
-            for(int j=0;j < quantityList.size();j++){
+            for(int i =0; i< mList.size();i++){
+                Cart_Model showViewModel = new Cart_Model();
+                //model = mList.get(Integer.toString(i));
+                showViewModel = mList.get(Integer.toString(keys.get(i)));
+                TotalQuantity +=  Integer.valueOf(showViewModel.getQuantity());
+                TotalBill += Integer.valueOf(showViewModel.getSubFoodPrice());
+            }
+            showQuanity.setText(Integer.toString(TotalQuantity));
+            showBill.setText(Integer.toString(TotalBill));
+
+//            for(int j=0;j < quantityList.size();j++){
+//
+//                for(String key : quantityList.keySet()){
+//                    quantityKeys.add(Integer.valueOf(key));
+//                    break;
+//                }
+//                int index = quantityKeys.get(Integer.valueOf(j));
+//                String b = quantityList.get(Integer.toString(index));
+//               // String b =  quantityKeysNew.get(position);
+//                if(b == null){
+//                    continue;
+//                }
+//                //else if(b.equals(position )){
+//                    String newValue = quantityList.get(position);
+//                    if(TotalQuantity == Integer.valueOf(originalQuantity) && checkQantity == false){
+//
+//                    }
+////                    else if(newValue.equals(qty) && TotalQuantity > Integer.valueOf(originalQuantity)){
+////                        //TotalQuantity -= Integer.valueOf(qty);
+////                        TotalQuantity -= Integer.valueOf("1");
+////                        if(TotalQuantity == 1){
+////                            checkQantity = false;
+////                        }
+////                    }
+//                    else if(qty != originalQuantity && TotalQuantity > Integer.valueOf(originalQuantity)){
+//                        //TotalQuantity -= Integer.valueOf(qty);
+//                        TotalQuantity -= Integer.valueOf("1");
+//                        if(TotalQuantity == 1){
+//                            checkQantity = false;
+//                        }
+//                    }
+//
+//                    else{
+//
+//                        //TotalQuantity += Integer.valueOf(qty);
+//
+//                        puraniValue += Integer.valueOf(newValue);
+//                        TotalQuantity += Integer.valueOf(newValue);
+//
+//                        if(Integer.valueOf(newValue) == 4){
+//                            //TotalQuantity = 3;
+//                            Toast.makeText(this, "You can only add 3 items per Food", Toast.LENGTH_SHORT).show();
+//                        }
+//                        checkQantity = true;
+//
+//                    }
+//                //}
+//
+//            }
+
+
+            //  showBill.setText(Integer.toString(TotalBill));
+            //String c = "";
+
+
+
+
+
+
+//            boolean check12 = false;
+////            TotalBill = 0;
+//            TotalQuantity = 0;
+//            for ( String key : quantityList.keySet() ) {
+//
+//
+//
+//
+//
+//
+//
+//                String cd="";
+//                for(int i = 0;i < quantityKeys.size();i++){
+//                    if(quantityKeys.get(i).equals(key)){
+//                        break;
+//
+//                    }
+//                    else {
+//                        continue;
+//                    }
+//                }
+//
+
+//                int j =  quantityKeys.get(Integer.valueOf(key));
+//                if(key.equals(quantityKeys.get(Integer.valueOf(key)))){
+//
+//                }
+//                else {
+//                    break;
+//                }
+//                try{
+//                  boolean n =   key.equals(quantityKeys.get(Integer.valueOf(position)));
+//                }
+//                catch(Exception e){
+//
+//
+//                }
+
+
+            }
+
+//            for(int i =0;i < mList.size(); i++){
+//
+//                 String m = Integer.toString(quantityKeys.get(i));
+//                Cart_Model mainModel = mList.get(m);
+//                TotalQuantity += Integer.valueOf(mainModel.getQuantity());
+//            }
+//
+//            showQuanity.setText(Integer.toString(TotalQuantity));
+//
+//
+//
+
+
+//            for(int j=0;j < quantityList.size();j++){
+//                // String b =  quantityList.get(position);
+////                if(quantityList.get(position).equals(qty)){
+//
+//                String b =  Integer.toString(quantityKeys.get(j));
+//                if(b.equals(position)){
+//                    String puranivalue = quantityList.get(Integer.valueOf(position));
+//                    if(TotalQuantity == Integer.valueOf(originalQuantity) && checkQantity == false){
+//
+//                    }
+//                    else if(puranivalue.equals(qty) && TotalQuantity > Integer.valueOf(originalQuantity)){
+//                        TotalQuantity -= Integer.valueOf(qty);
+//                        if(TotalQuantity == 1){
+//                            checkQantity = false;
+//                        }
+//                    }
+//                    else{
+//
+//                        TotalQuantity += Integer.valueOf(qty);
+//                        if(TotalQuantity == 4){
+//                            TotalQuantity = 3;
+//                        }
+//                        checkQantity = true;
+//                    }
+//                }
+//
+//            }
+
+
+            //  showBill.setText(Integer.toString(TotalBill));
+            //String c = "";
+
+
+
+//            for ( int key : quantityList.keySet() ) {
+//                quantityKeys.add(Integer.valueOf(key));
+//                break;
+//
+//            }
+//
+//            for(int j=0;j < mList.size();j++) {
+//                if(quantityList.get(Integer.valueOf(position)).equals(qty)) {
+//                    TotalQuantity += Integer.valueOf(qty);
+//
+//                }
+//                else if(Integer.valueOf(qty) <  Integer.valueOf(quantityList.get(position))){
+//                    TotalQuantity -= Integer.valueOf(qty);
+//                }
+//                else {
+//                    TotalQuantity += Integer.valueOf(qty);
+//                }
+//
+//
+////               Cart_Model mainModel = mList.get(position);
+////               TotalQuantity +=  Integer.valueOf(mainModel.getQuantity());
+////               String cc = "";
+//
+//
+//            }
+
+
+
                 // String b =  quantityList.get(position);
 //                if(quantityList.get(position).equals(qty)){
 
-                String b =  Integer.toString(quantityKeys.get(j));
-                if(b.equals(position)){
-                     String puranivalue = quantityList.get(Integer.valueOf(position));
-                     if(TotalQuantity == Integer.valueOf(originalQuantity) && checkQantity == false){
-
-                     }
-                    else if(puranivalue.equals(qty) && TotalQuantity > Integer.valueOf(originalQuantity)){
-                         TotalQuantity -= Integer.valueOf(qty);
-                         if(TotalQuantity == 1){
-                             checkQantity = false;
-                         }
-                     }
-                     else{
-
-                         TotalQuantity += Integer.valueOf(qty);
-                         if(TotalQuantity == 4){
-                             TotalQuantity = 3;
-                         }
-                         checkQantity = true;
-                     }
-                }
-
-            }
-
-            showQuanity.setText(Integer.toString(TotalQuantity));
-            //  showBill.setText(Integer.toString(TotalBill));
-            String c = "";
 
 
-
-            /////////////
-
-
-//            for (int i = 0; i < postion_list.size(); i++) {
-//
-//
-//                for (int j = 0; j < quantityList.size(); j++) {
-//
-//                    TotalQuantity += Integer.valueOf(quantityList.get(Integer.valueOf(position)));
-//                    TotalBill += Integer.valueOf(priceList.get(Integer.valueOf(position)));
-//
-//                }
-//                showQuanity.setText(Integer.toString(TotalQuantity));
-//                showBill.setText(Integer.toString(TotalBill));
-//                String c = "";
-//
+//                if(TotalQuantity == 0){
+//                TotalQuantity += Integer.valueOf(qty);
+//            }
+//            else if(TotalQuantity > Integer.valueOf(qty)){
 //
 //            }
 
 
 
-            //  Is algo ko likhne mai bht time laga or fazool tha easy logic laga ke kam aadhe ghnte mai hogya :D
-            //  well not really :p
 
 
 
 
-//a
-//            if(quantityList.size() == 1){
-//                for(int i=Integer.valueOf(position); i<= quantityList.size(); i++){
-//                    if(quantityList.get(i) == null){
-//                        continue;
-//                    }
-//                    TotalQuantity += Integer.valueOf(quantityList.get(i));
-//                    TotalBill += Integer.valueOf(priceList.get(i));
-//                }
-//
-//                showQuanity.setText(Integer.toString(TotalQuantity));
-//                showBill.setText(Integer.toString(TotalBill));
-//                String c = "";
-//
-//
-//            }
-//
-//           else if(quantityList.size() > 1) {
-//               int j = 0;
-//               for(int y = postion_list.get(j); y < postion_list.size();y++){
-//
-//                   for(int i=0; i< quantityList.size(); i++){
-////
-//                    TotalQuantity += Integer.valueOf(quantityList.get(i));
-//                    TotalBill += Integer.valueOf(priceList.get(i));
-//                }
-//
-//                showQuanity.setText(Integer.toString(TotalQuantity));
-//                showBill.setText(Integer.toString(TotalBill));
-//                String c = "";
-//                j++;
-//
-//               }
 
 
-//                for(int i=0; i< quantityList.size(); i++){
-////
-////                    TotalQuantity += Integer.valueOf(quantityList.get(i));
-////                   // TotalBill += Integer.valueOf(priceList.get(i));
-////                }
-
-
-        }
+       // }
+        //showQuanity.setText(Integer.toString(TotalQuantity));
 
 
 
-
-        
-
-
-        String c ="";
 
     }
     private void setCartRecyclerView(){
